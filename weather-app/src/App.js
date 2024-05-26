6a0763604c1831f58886798cd529d2ceadf6c77e
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-05-26 21:03:56 +0300

    assets

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+// const url ='https://api.openweathermap.org/data/2.5/weather?lat=5&lon=42&appid=401186890c82b3946e39382041a4e4a1'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className='top'>
+          <div className="location">
+            <p>Nairobi</p>
+          </div>
+          <div className="temp">
+            <h1>19°C</h1>
+            <div className="description">
+              <p>Windy</p>
+            </div>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>25°C</p>
+          </div>
+          <div className="humidity">
+            <p>87%</p>
+          </div>
+          <div className="wind">
+            <p>18 km/h</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
